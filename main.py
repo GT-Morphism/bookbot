c5edef9c0aa3fc5dc3026c66c6aa59c8c3869bca
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2023-12-04 23:00:23 +0100

    :sparkles: Get number of words in text file

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Good evening, Don")
+def main():
+	with open("./books/frankenstein.txt") as f:
+		file_contents = f.read()
+		num_words = get_num_of_words(file_contents)
+		print(num_words)
+	
+def get_num_of_words(text):
+	return len(text.split())
+
+main()
+
+
