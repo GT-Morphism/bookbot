c2c313807af39ff6fff13f265453894deb0c2140
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2023-12-04 23:41:55 +0100

    :sparkles: Add report

--- main.py
+++ main.py
@@ -2,16 +2,28 @@ def main():
 	with open("./books/frankenstein.txt") as f:
 		file_contents = f.read()
 		num_words = get_num_of_words(file_contents)
-		print(num_words)
-		letters_count = get_letters_count(file_contents)
-		print(letters_count)
-	
+		letters_count = get_letters_count(file_contents)	
+		letters_count_sorted = sorted(letters_count.items(), key=lambda x:x[1], reverse=True)
+
+		print("--- Begin report of books/frankenstein.txt ---")
+		print("\n")
+		print(f"{num_words} words found in the document.")
+		print("\n")
+
+		for letter_count in letters_count_sorted:
+			print(f"The '{letter_count[0]}' character was found {letter_count[1]} times.")
+
+		print("\n")
+		print("--- End report ---")
+
 def get_num_of_words(text):
 	return len(text.split())
 
 def get_letters_count(text):
 	letters_count = {}
 	for char in text.lower():
+		if (not char.isalpha()):
+			continue
 		if (char not in letters_count):
 			letters_count[char] = 0
 		letters_count[char] += 1
