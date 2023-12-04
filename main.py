07cf75bbb5d96184f4eb69ad59475b18e21efe81
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2023-12-04 23:08:50 +0100

    :sparkles: Get letters count

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ def main():
 		file_contents = f.read()
 		num_words = get_num_of_words(file_contents)
 		print(num_words)
+		letters_count = get_letters_count(file_contents)
+		print(letters_count)
 	
 def get_num_of_words(text):
 	return len(text.split())
+
+def get_letters_count(text):
+	letters_count = {}
+	for char in text.lower():
+		if (char not in letters_count):
+			letters_count[char] = 0
+		letters_count[char] += 1
+	return letters_count
+
 
 main()
 
